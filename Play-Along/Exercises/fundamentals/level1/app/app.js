aa312c5e42b6d9681ac1426d64bb9bbe0307f59c
Jaga1998 <44741841+Jaga1998@users.noreply.github.com>
2020-05-23 22:00:00 +0530

    Update app.js

--- Play-Along/Exercises/fundamentals/level1/app/app.js
+++ Play-Along/Exercises/fundamentals/level1/app/app.js
@@ -22,6 +22,7 @@ function onLoadClickEventHandler() {
 		var email = $('#email').val();				// Email id of the user, creating the ticket
 		if (title && desc && email) {
 			//createFreshdeskTicket(title, desc, email);
+			
 		} else {
 			showNotification('danger', 'Ticket Values cannot empty, Fill all values');
 		}
@@ -30,7 +31,30 @@ function onLoadClickEventHandler() {
 
 // 1.2 Paste the code for createFreshdeskTicket() here 👇🏼 !
 
-
+function createFreshdeskTicket(title, description, email) {
+  client.request.post("https://<%=iparam.freshdesk_subdomain%>.freshdesk.com/api/v2/tickets", {
+    headers: {
+      Authorization: "Basic <%= encode(iparam.freshdesk_api_key)%>",
+      "Content-Type": "application/json;charset=utf-8"
+    },
+    body: JSON.stringify({
+      description: `${description}`,
+      email: `${email}`,
+      priority: 1,
+      status: 2,
+      subject: `${title}`
+    })
+  })
+    .then(function () {
+      showNotification('success', 'Ticket is successfully created');
+      //Clears user input after posting data
+      clearInputfields();
+    })
+    .catch(function (error) {
+      console.error(error);
+      showNotification('danger', 'Unable to create ticket');
+    });
+}
 
 /**
  * Function to show notifications to the user
